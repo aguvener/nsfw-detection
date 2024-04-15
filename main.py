ff5c5fcd58c13403d9aa2558930855e0c76eebf0
aguvener <90110302+aguvener@users.noreply.github.com>
2024-04-15 12:52:28 +0300

    Update image processing script to accept command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+"""
+This script is used to process images in a directory and classify them as NSFW or SFW.
+The script uses a pre-trained model to classify images and copy them to the output directory if they are NSFW.
+If an error occurs while processing an image, the image is copied to the error directory.
+
+Usage: python main.py -I <input_directory> -O <output_directory> -E <error_directory>
+
+Arguments:
+    -I, --input: Input directory containing images to be processed.
+    -O, --output: Output directory to copy NSFW images.
+    -E, --error: Error directory to copy images that could not be processed.
+"""
+
 import os
 import shutil
 import torch
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 from transformers import AutoModelForImageClassification, ViTImageProcessor
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
+import argparse
 
 # Initialize model and device
 model = AutoModelForImageClassification.from_pretrained("Falconsai/nsfw_image_detection")
@@ -13,13 +27,22 @@ model.eval()
 device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 model.to(device)
 
-# Define the directories
-base_directory = '/Users/aguvener/Desktop/imgtag/photo-similarity-search/photos'
-nsfw_directory = 'nsfw_directory'
-error_directory = 'error_directory'
-os.makedirs(nsfw_directory, exist_ok=True)
-os.makedirs(error_directory, exist_ok=True)
+# Get directories from command line arguments
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-I', '--input', help='Input directory', required=True)
+parser.add_argument('-O', '--output', help='Output directory')
+parser.add_argument('-E', '--error', help='Error directory')
+args = parser.parse_args()
 
+# Set default values if directories are not specified
+output_directory = args.output if args.output else 'output'
+nsfw_directory = f'{output_directory}/nsfw'
+error_directory = args.error if args.error else f'{output_directory}/error'
+
+# Create directories if they don't exist
+os.makedirs(output_directory, exist_ok=True)
+os.makedirs(error_directory, exist_ok=True)
 
 # Function to process each image and copy if necessary
 def process_image(image_path):
@@ -46,7 +69,7 @@ def process_images(image_paths):
 # Collect all image paths
 image_files = [
     os.path.join(root, file)
-    for root, _, files in os.walk(base_directory)
+    for root, _, files in os.walk(args.input)
     for file in files
     if file.lower().endswith(('png', 'jpg', 'jpeg', 'bmp', 'gif'))
 ]
